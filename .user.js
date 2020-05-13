f551d5507c1754233e665fd7969ffd9be2dc3cd9
slin10 <nleterrier@neuf.fr>
2020-05-13 16:00:49 +0200

    Update .user.js

--- .user.js
+++ .user.js
@@ -10,13 +10,13 @@
 // @connect      *Zorn192.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
-// @grant        GM_xmlhttpRequest 
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'C:\Users\Nils\Documents\GitHub\AutoTrimps\AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
